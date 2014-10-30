54e7f56cc30e78b032a95502a50aaa6bc854f932
Apo <apolonski@gmail.com>
2014-10-30 19:51:19 +0100

    refactored game state to support joining

--- src/wonders7/game-logic/game-state.clj
+++ src/wonders7/game-logic/game-state.clj
@@ -16,30 +16,38 @@
 
 ; fresh game state template
 (def initial-state
-  {1 {:hand {}, :table #{}, :cash 3, :war-score 0}
-   2 {:hand {}, :table #{}, :cash 3, :war-score 0}
-   3 {:hand {}, :table #{}, :cash 3, :war-score 0}})
-
-; runs a function on each of the values in a map
-(defn map-vals [m f]
-  (into {} (for [[k v] m] [k (f v)])))
+  {:players {}
+   :trash #{}
+   :age 1
+   :free-seats 7
+   :in-progress false})
 
 ; create refs on the inner keys in the game state
-(def current-state (map-vals initial-state #(map-vals % ref)))
+(def current-state
+  (into {} (for [[k v] initial-state] [k (ref v)])))
+
+; adds an entry to the players map located in the current game state
+(defn join-game [player-name]
+  (dosync
+    (when-not @(get current-state :in-progress)
+      (let [free-seats (get current-state :free-seats)]
+        (when (> @free-seats 0)
+          (alter (get current-state :players) into [[(- 8 @free-seats) {:name player-name :hand (ref {}) :table (ref #{}) :cash (ref 3) :war-score (ref 0)}]])
+          (alter free-seats dec))))))
 
 ; hepler for inc/dec cash or war-score, must be called in a transaction
 (defn gain [player quantity subject]
-  (alter (get-in current-state [player subject]) + quantity))
+  (alter (get-in @(get current-state :players) [player subject]) + quantity))
 
 ; helper for populating hands
 (defn deal [& {:keys [age] :or {age 1}}]
-  (loop [card-pool (shuffle (get-deck age)) player-pool (keys current-state)]
+  (loop [card-pool (shuffle (get-deck age)) player-pool (keys @(get current-state :players))]
     (if
       (empty? player-pool) nil
       (do
         (dosync
           (alter
-            (get-in current-state [(first player-pool) :hand])
+            (get-in @(get current-state :players) [(first player-pool) :hand])
             (fn [x] (apply merge-with + (map (fn [y] {y 1}) (take 7 card-pool))))))
         (recur (drop 7 card-pool) (drop 1 player-pool))))))
 
@@ -47,15 +55,23 @@
 (defn play [& {:keys [player card sell] :or {sell false}}]
   (let [card-effect (get-in cards [card :effect])]
     (dosync
-      (alter (get-in current-state [player :hand]) (fn [x] (into {} (filter #(> (second %) 0) (update-in x [card] dec)))))
+      (alter (get-in @(get current-state :players) [player :hand]) (fn [x] (into {} (filter #(> (second %) 0) (update-in x [card] dec)))))
       (if sell
-        (gain player 3 :cash)
         (do
-          (alter (get-in current-state [player :table]) conj card)
+          (gain player 3 :cash)
+          (alter (get current-state :trash) conj card))
+        (do
+          (alter (get-in @(get current-state :players) [player :table]) conj card)
           (when-not (nil? card-effect) (eval (conj (drop 1 card-effect) player (first card-effect)))))))))
 
-current-state
+(join-game "apo")
+(join-game "wuj")
+(join-game "zoll")
+
+;(start-game)
 
 (deal :age 1)
 
-(play :card "Baths" :player 2 :sell true)
+current-state
+
+;(play :card "Altar" :player 1)
